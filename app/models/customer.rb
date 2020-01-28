7726a91d7bc311d33037c33f82d0a748ad5d4fb2
Dylan Connolly <dylanconnolly145@gmail.com>
2020-01-28 11:47:00 -0700

    Add depedent destroy to models so data can be reloaded

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,4 @@
 class Customer < ApplicationRecord
-  has_many :invoices
+  has_many :invoices, :dependent => :destroy
 
 end
--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,7 +2,8 @@ class Invoice < ApplicationRecord
   belongs_to :customer
   belongs_to :merchant
 
-  has_many :items, through: :invoice_items
+  has_many :invoice_items, :dependent => :destroy
+  has_many :items, through: :invoice_items, :dependent => :destroy
 
   enum status: ["unshipped", "shipped"]
 end

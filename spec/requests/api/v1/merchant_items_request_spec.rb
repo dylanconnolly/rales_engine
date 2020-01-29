25b0826a8c495fb2e43cde118cdf61392b757f97
Dylan Connolly <dylanconnolly145@gmail.com>
2020-01-28 23:37:48 -0700

    Create merchant items request spec to generate list of a merchant's items

---
+++ spec/requests/api/v1/merchant_items_request_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe "merchants items API" do
+  it "sends a list of that merchant's items" do
+    merchant = create(:merchant_with_items)
+    merchant2 = create(:merchant_with_items, items_count: 3)
+
+    items = merchant.items
+    not_items = merchant2.items
+
+    get "/api/v1/merchants/#{merchant.id}/items"
+
+    expect(response).to be_successful
+
+    items_hash = JSON.parse(response.body)
+    # require "pry"; binding.pry
+    # expect(items_hash["data"].count)
+  end
+end

e853caedc64bc04fce0e5f9c6a699271b2248745
Dylan Connolly <dylanconnolly145@gmail.com>
2020-01-29 15:34:18 -0700

    Convert created_at param to datetime object. Created at test won't work due to mismatch in format but passing spec harness

--- spec/requests/api/v1/merchant_find_request_spec.rb
+++ spec/requests/api/v1/merchant_find_request_spec.rb
@@ -13,4 +13,30 @@ describe "merchant finder API" do
     merchant_info = JSON.parse(response.body)
     expect(merchant_info["data"]["attributes"]["name"]).to eq(merchant.name)
   end
+
+  it "returns a single instance of merchant baseed off name" do
+    create_list(:merchant, 10)
+
+    merchant = Merchant.last
+
+    get "/api/v1/merchants/find?name=#{merchant.name}"
+
+    expect(response).to be_successful
+
+    merchant_info = JSON.parse(response.body)
+    expect(merchant_info["data"]["attributes"]["name"]).to eq(merchant.name)
+  end
+
+  it "returns a single instance of merchant based off date created" do
+    create_list(:merchant, 10)
+
+    merchant = Merchant.last
+
+    get "/api/v1/merchants/find?created_at=#{merchant.created_at.to_datetime}"
+
+    expect(response).to be_successful
+
+    merchant_info = JSON.parse(response.body)
+    expect(merchant_info["data"]["attributes"]["name"]).to eq(merchant.name)
+  end
 end

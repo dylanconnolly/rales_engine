788827cda7fccc0735a984787d3666d1de3c2699
Dylan Connolly <dylanconnolly145@gmail.com>
2020-01-29 14:35:09 -0700

    Separate AR query into multiple lines for readability

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,4 +2,12 @@ class Merchant < ApplicationRecord
   has_many :items, :dependent => :destroy
   has_many :invoices, :dependent => :destroy
   has_many :transactions, through: :invoices
+
+  def self.most_revenue(number_of_records)
+    Merchant.joins(invoices: [:invoice_items, :transactions]).
+    select('merchants.*, SUM(invoice_items.unit_price * invoice_items.quantity) AS total_revenue').
+    group(:id).
+    order('total_revenue desc').
+    limit(number_of_records)
+  end
 end

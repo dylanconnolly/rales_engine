aa56b46c9970741041c59551768c269c0224f384
Dylan Connolly <dylanconnolly145@gmail.com>
2020-01-28 22:45:54 -0700

    Update routes to use module scope for place merchant item controller action in a merchant directory

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,14 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :merchants, only: [:index, :show] do
-        resources :items, only: [:index]
+
+        scope module: 'merchant' do
+          resources :items, only: [:index]
+        end
+
       end
+
+
     end
   end
 end

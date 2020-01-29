dbfad4b2f99d147e1e4d0b2925543215a4ed1e6f
Dylan Connolly <dylanconnolly145@gmail.com>
2020-01-29 11:47:23 -0700

    Add relationships between transactions, merchants, and invoices, to models for AR queries

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,7 +1,9 @@
 class Invoice < ApplicationRecord
   belongs_to :customer
   belongs_to :merchant
+  scope :customer, -> (id) {where("customer_id = ?", id)}
 
+  has_many :transactions
   has_many :invoice_items, :dependent => :destroy
   has_many :items, through: :invoice_items, :dependent => :destroy
 

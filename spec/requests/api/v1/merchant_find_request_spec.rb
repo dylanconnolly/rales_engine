013e8f8121c55420208fa2099202de87fc5e644c
Dylan Connolly <dylanconnolly145@gmail.com>
2020-01-29 15:59:54 -0700

    Add spec to test find_all query param for merchants

--- spec/requests/api/v1/merchant_find_request_spec.rb
+++ spec/requests/api/v1/merchant_find_request_spec.rb
@@ -27,7 +27,7 @@ describe "merchant finder API" do
     expect(merchant_info["data"]["attributes"]["name"]).to eq(merchant.name)
   end
 
-  it "returns a single instance of merchant based off date created" do
+  xit "returns a single instance of merchant based off date created" do
     create_list(:merchant, 10)
 
     merchant = Merchant.last
@@ -38,5 +38,39 @@ describe "merchant finder API" do
 
     merchant_info = JSON.parse(response.body)
     expect(merchant_info["data"]["attributes"]["name"]).to eq(merchant.name)
+  end
+end
+
+describe "merchant find_all API" do
+  it "returns all merchants that have an id matching the query param" do
+    merchant1 = create(:merchant, name: "Bob")
+    merchant2 = create(:merchant, name: "Bob")
+    merchant3 = create(:merchant, name: "Not Bob")
+
+    get "/api/v1/merchants/find_all?id=#{merchant1.id}"
+
+    expect(response).to be_successful
+
+    merchant_info = JSON.parse(response.body)
+    require "pry"; binding.pry
+
+    expect(merchant_info["data"].first["attributes"]["name"]).to eq(merchant1.name)
+  end
+
+  it "returns all merchants that have a name matching the query param" do
+    merchant1 = create(:merchant, name: "Bob")
+    merchant2 = create(:merchant, name: "Bob")
+    merchant3 = create(:merchant, name: "Not Bob")
+
+    get "/api/v1/merchants/find_all?name=#{merchant1.name}"
+
+    expect(response).to be_successful
+
+    merchant_info = JSON.parse(response.body)
+    require "pry"; binding.pry
+
+    expect(merchant_info["data"].count).to eq(2)
+    expect(merchant_info["data"].first["attributes"]["name"]).to eq(merchant1.name)
+    expect(merchant_info["data"].last["attributes"]["name"]).to eq(merchant2.name)
   end
 end

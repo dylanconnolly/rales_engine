885a5d3f7eb83ab7b0378e73c53c77badfefe22d
Dylan Connolly <dylanconnolly145@gmail.com>
2020-01-30 15:05:03 -0700

    Create spec for revenue on date api

--- spec/requests/api/v1/merchant_revenue_requests_spec.rb
+++ spec/requests/api/v1/merchant_revenue_requests_spec.rb
@@ -20,6 +20,19 @@ describe "merchant API" do
   end
 
   it "returns total revenue across all merchants on a specified date" do
-    
+    create_list(:invoice, 10)
+
+    Invoice.all.each do |invoice|
+      create(:invoice_item, invoice: invoice, item: (create(:item, merchant: invoice.merchant)))
+      create(:transaction, invoice: invoice)
+    end
+
+    get "/api/v1/merchants/revenue?date=#{Date.today.strftime('%Y-%m-%d')}"
+
+    expect(response).to be_successful
+
+    query = JSON.parse(response.body)
+
+    expect(query["data"]["attributes"]["total_revenue"]).to eq("10.0")
   end
 end

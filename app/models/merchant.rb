87bb17b3ea61313431d084e73741d7a3f5abf6cb
Dylan Connolly <dylanconnolly145@gmail.com>
2020-01-30 15:04:01 -0700

    Add most_revenue_on_date(date) class method to return total revenue across all merchants on that date

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -7,7 +7,19 @@ class Merchant < ApplicationRecord
     Merchant.joins(invoices: [:invoice_items, :transactions]).
     select('merchants.*, SUM(invoice_items.unit_price * invoice_items.quantity) AS total_revenue').
     group(:id).
+    merge(Transaction.successful).
     order('total_revenue desc').
     limit(number_of_records)
+  end
+
+  def self.total_revenue_on_date(date)
+    starttime = Time.zone.parse(date)
+    endtime = starttime + 1.day
+    Merchant.joins(invoices: [:invoice_items, :transactions]).
+    select("date_trunc('day', invoice_items.created_at) AS day, SUM((invoice_items.unit_price * invoice_items.quantity)/100.0) AS total_revenue").
+    group('day').
+    merge(Transaction.successful).
+    where("invoices.created_at BETWEEN '#{starttime}' AND '#{endtime}'").
+    limit(1)
   end
 end

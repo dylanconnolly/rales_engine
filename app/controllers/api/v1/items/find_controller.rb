b8f8fbf4e9281032b7ed5505e6d50ebf087c9d17
Dylan Connolly <dylanconnolly145@gmail.com>
2020-02-01 10:30:49 -0700

    Add private method converted_params to convert unit_price to cents if that param exists

--- app/controllers/api/v1/items/find_controller.rb
+++ app/controllers/api/v1/items/find_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::Items::FindController < ApplicationController
 
   def index
-    item = Item.order(:id).find_by(search_params)
+    item = Item.order(:id).find_by(converted_params)
     
     render json: ItemSerializer.new(item)
   end
@@ -10,6 +10,14 @@ class Api::V1::Items::FindController < ApplicationController
 
     def search_params
       params.permit(:id, :name, :description, :unit_price, :created_at, :updated_at)
+    end
+
+    def converted_params
+      converted_params = search_params.clone
+      if search_params.keys.include?("unit_price")
+        converted_params["unit_price"] = (search_params["unit_price"].to_f * 100).to_i
+      end
+      converted_params
     end
 
 end

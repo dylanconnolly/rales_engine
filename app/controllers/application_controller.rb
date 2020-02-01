67ecca47b0e2a2073b4477f7efcc1daea9701dca
Dylan Connolly <dylanconnolly145@gmail.com>
2020-02-01 11:04:03 -0700

    Move converted_params method to application controller so it can be called across multiple controllers. Update converted_params method to round up when converting from float to integer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+
+  def converted_params
+    converted_params = search_params.clone
+    if search_params.keys.include?("unit_price")
+      converted_params["unit_price"] = (search_params["unit_price"].to_f * 100).ceil
+    end
+    converted_params
+  end
 end

aa76a8e17ccff5062f165b4bb1e8a70152f78517
Dylan Connolly <dylanconnolly145@gmail.com>
2020-02-01 21:05:38 -0700

    Add test for most_revenue model method for item

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,13 @@ class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items, :dependent => :destroy
   has_many :invoices, through: :invoice_items, :dependent => :destroy
+
+  def self.most_revenue(number_of_records)
+    joins(invoices: [:invoice_items, :transactions]).
+    select('items.*, SUM(invoice_items.unit_price * invoice_items.quantity) AS total_revenue').
+    group(:id).
+    merge(Transaction.successful).
+    order('total_revenue desc').
+    limit(number_of_records)
+  end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
-# RSpec.describe Item, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe Item, type: :model do
+  it "most_revenue" do
+    create_list(:item, 5)
+
+    top_item = Item.last
+
+    number_of_invoice_items = 1
+    Item.all.each do |item|
+      create_list(:invoice_item, number_of_invoice_items, item: item)
+      number_of_invoice_items += 1
+    end
+
+    Invoice.all.each do |invoice|
+      create(:transaction, invoice: invoice)
+    end
+
+    top_items = Item.most_revenue(5)
+    
+    expect(top_items.length).to eq(5)
+    expect(top_items.first).to eq(top_item)
+    expect(Item.most_revenue(2).length).to eq(2)
+  end
+end

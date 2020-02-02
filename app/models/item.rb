afe1b417752c38c4108140a9c2f970a6a08ddae5
Dylan Connolly <dylanconnolly145@gmail.com>
2020-02-01 22:34:43 -0700

    Add best_day class method in item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,4 +11,20 @@ class Item < ApplicationRecord
     order('total_revenue desc').
     limit(number_of_records)
   end
+
+  def self.best_day(item_id)
+    Item.joins(invoices: :transactions).
+    select("COUNT(invoices.created_at), date_trunc('day', invoices.created_at) AS best_day").
+    group('invoices.created_at').
+    merge(Transaction.successful).
+    where("items.id = #{item_id}").
+    order('count desc, best_day desc').
+    limit(1)
+  end
 end
+
+# Item.joins(invoices: :transactions).
+# select("COUNT(invoices.created_at), date_trunc('day', invoices.created_at) AS best_day").
+# group('invoices.created_at').
+# merge(Transaction.successful).
+# where("items.id = 1099").to_sql

27f80c0822cf4369e183c124f1c4c72dc984f410
Dylan Connolly <dylanconnolly145@gmail.com>
2020-02-01 18:00:38 -0700

    Update spec to have tests for find_all by each item attribute

--- spec/requests/api/v1/items/find_request_spec.rb
+++ spec/requests/api/v1/items/find_request_spec.rb
@@ -111,85 +111,123 @@ describe "item single finder endpoints" do
 end
 
 describe "item find_all request" do
-  xit "returns all items that have an id matching the query param" do
-    merchant1 = create(:merchant, name: "Bob")
-    merchant2 = create(:merchant, name: "Scott")
-    merchant3 = create(:merchant, name: "Not Bob")
+  it "returns all items that have an id matching the query param" do
+    item1 = create(:item)
+    item2 = create(:item)
+    item3 = create(:item)
 
-    get "/api/v1/items/find_all?id=#{merchant1.id}"
+    get "/api/v1/items/find_all?id=#{item1.id}"
 
     expect(response).to be_successful
 
-    merchant_info = JSON.parse(response.body)
+    item1_info = JSON.parse(response.body)
 
-    expect(merchant_info["data"].first["attributes"]["name"]).to eq(merchant1.name)
+    expect(item1_info["data"].first["attributes"]["id"]).to eq(item1.id)
+
+    get "/api/v1/items/find_all?id=#{item2.id}"
+
+    item2_info = JSON.parse(response.body)
+
+    expect(item2_info["data"].first["attributes"]["id"]).to eq(item2.id)
   end
 
 
-  xit "returns all items that have a name matching the query param" do
-    merchant1 = create(:merchant, name: "Bob")
-    merchant2 = create(:merchant, name: "Bob")
-    merchant3 = create(:merchant, name: "Not Bob")
+  it "returns all items that have a name matching the query param" do
+    item1 = create(:item, name: "Cool Item")
+    item2 = create(:item, name: "Cool Item")
+    item3 = create(:item)
 
-    get "/api/v1/items/find_all?name=#{merchant1.name}"
+    get "/api/v1/items/find_all?name=#{item1.name}"
 
     expect(response).to be_successful
 
-    merchant_info = JSON.parse(response.body)
+    item_info = JSON.parse(response.body)
 
-    expect(merchant_info["data"].count).to eq(2)
-    expect(merchant_info["data"].first["attributes"]["name"]).to eq(merchant1.name)
-    expect(merchant_info["data"].last["attributes"]["name"]).to eq(merchant2.name)
+    expect(item_info["data"].count).to eq(2)
+    expect(item_info["data"].first["attributes"]["id"]).to eq(item1.id)
+    expect(item_info["data"].last["attributes"]["id"]).to eq(item2.id)
   end
 
-  xit "returns all items that were created on a specific date" do
-    merchant1 = create(:merchant, name: "Bob", created_at: "2020-01-10", updated_at: "2020-01-25")
-    merchant2 = create(:merchant, name: "Stove", created_at: "2020-01-10", updated_at: "2020-01-31")
-    merchant3 = create(:merchant, name: "Not Bob", created_at: "2020-01-12", updated_at: "2020-01-25")
+  it "retuns all items that have a description matching query param" do
+    item1 = create(:item, description: "Item is a good item")
+    item2 = create(:item, description: "Item is a good item")
+    item3 = create(:item)
 
-    get "/api/v1/items/find_all?created_at=#{merchant1.created_at}"
-
-    expect(response).to be_successful
-
-    merchant_info = JSON.parse(response.body)
-
-    expect(merchant_info["data"].count).to eq(2)
-    expect(merchant_info["data"].first["attributes"]["name"]).to eq(merchant1.name)
-    expect(merchant_info["data"].last["attributes"]["name"]).to eq(merchant2.name)
-
-    get "/api/v1/items/find_all?created_at=#{merchant3.created_at}"
+    get "/api/v1/items/find_all?description=#{item1.description}"
 
     expect(response).to be_successful
 
-    merchant_info = JSON.parse(response.body)
+    item_info = JSON.parse(response.body)
 
-    expect(merchant_info["data"].count).to eq(1)
-    expect(merchant_info["data"].first["attributes"]["name"]).to eq(merchant3.name)
+    expect(item_info["data"].count).to eq(2)
+    expect(item_info["data"].first["attributes"]["id"]).to eq(item1.id)
+    expect(item_info["data"].last["attributes"]["id"]).to eq(item2.id)
   end
 
-  xit "returns all items that were created on a specific date" do
-    merchant1 = create(:merchant, name: "Bob", created_at: "2020-01-10", updated_at: "2020-01-25")
-    merchant2 = create(:merchant, name: "Stove", created_at: "2020-01-10", updated_at: "2020-01-14")
-    merchant3 = create(:merchant, name: "Not Bob", created_at: "2020-01-12", updated_at: "2020-01-25")
+  it "retuns all items that have a unit price matching query param" do
+    item1 = create(:item, unit_price: 21689)
+    item2 = create(:item, unit_price: 21689)
+    item3 = create(:item)
 
-    get "/api/v1/items/find_all?updated_at=#{merchant1.updated_at}"
-
-    expect(response).to be_successful
-
-    merchant_info = JSON.parse(response.body)
-
-    expect(merchant_info["data"].count).to eq(2)
-    expect(merchant_info["data"].first["attributes"]["name"]).to eq(merchant1.name)
-    expect(merchant_info["data"].last["attributes"]["name"]).to eq(merchant3.name)
-
-    get "/api/v1/items/find_all?updated_at=#{merchant2.updated_at}"
+    get "/api/v1/items/find_all?unit_price=216.89"
 
     expect(response).to be_successful
 
-    merchant_info = JSON.parse(response.body)
+    item_info = JSON.parse(response.body)
 
-    expect(merchant_info["data"].count).to eq(1)
-    expect(merchant_info["data"].first["attributes"]["name"]).to eq(merchant2.name)
+    expect(item_info["data"].count).to eq(2)
+    expect(item_info["data"].first["attributes"]["id"]).to eq(item1.id)
+    expect(item_info["data"].last["attributes"]["id"]).to eq(item2.id)
+  end
+
+  it "returns all items that were created on a specific date" do
+    item1 = create(:item, created_at: "2020-01-10", updated_at: "2020-01-25")
+    item2 = create(:item, created_at: "2020-01-10", updated_at: "2020-01-31")
+    item3 = create(:item, created_at: "2020-01-12", updated_at: "2020-01-25")
+
+    get "/api/v1/items/find_all?created_at=#{item1.created_at}"
+
+    expect(response).to be_successful
+
+    item_info = JSON.parse(response.body)
+
+    expect(item_info["data"].count).to eq(2)
+    expect(item_info["data"].first["attributes"]["id"]).to eq(item1.id)
+    expect(item_info["data"].last["attributes"]["id"]).to eq(item2.id)
+
+    get "/api/v1/items/find_all?created_at=#{item3.created_at}"
+
+    expect(response).to be_successful
+
+    item_info = JSON.parse(response.body)
+
+    expect(item_info["data"].count).to eq(1)
+    expect(item_info["data"].first["attributes"]["id"]).to eq(item3.id)
+  end
+
+  it "returns all items that were updated on a specific date" do
+    item1 = create(:item, created_at: "2020-01-10", updated_at: "2020-01-25")
+    item2 = create(:item, created_at: "2020-01-10", updated_at: "2020-01-14")
+    item3 = create(:item, created_at: "2020-01-12", updated_at: "2020-01-25")
+
+    get "/api/v1/items/find_all?updated_at=#{item1.updated_at}"
+
+    expect(response).to be_successful
+
+    item_info = JSON.parse(response.body)
+
+    expect(item_info["data"].count).to eq(2)
+    expect(item_info["data"].first["attributes"]["id"]).to eq(item1.id)
+    expect(item_info["data"].last["attributes"]["id"]).to eq(item3.id)
+
+    get "/api/v1/items/find_all?updated_at=#{item2.updated_at}"
+
+    expect(response).to be_successful
+
+    item_info = JSON.parse(response.body)
+
+    expect(item_info["data"].count).to eq(1)
+    expect(item_info["data"].first["attributes"]["id"]).to eq(item2.id)
   end
 
 end

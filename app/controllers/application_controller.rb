fd95cb3849251336d5fceadcd2583a830506d378
Dylan Connolly <dylanconnolly145@gmail.com>
2020-02-02 13:25:24 -0700

    Add conditional to capitalize start of each sentence for an item description

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,8 +9,19 @@ class ApplicationController < ActionController::API
       converted_params["status"] = (search_params["status"].downcase)
     end
     if search_params.keys.include?("name")
-      converted_params["name"] = ((search_params["name"]).titleize)
+      converted_params["name"] = (search_params["name"].titleize)
+    end
+    if search_params.keys.include?("description")
+      converted_params["description"] = capitalize_start_of_each_sentence(search_params["description"])
     end
     converted_params
   end
+
+  private
+
+    def capitalize_start_of_each_sentence(description)
+      description.split('. ').map do |sentence|
+        sentence.capitalize
+      end.join('. ')
+    end
 end

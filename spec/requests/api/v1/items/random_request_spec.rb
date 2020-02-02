f08ff7f5c43720b90c466efcdbe34af74e14e544
Dylan Connolly <dylanconnolly145@gmail.com>
2020-02-01 18:18:06 -0700

    Create random item spec

---
+++ spec/requests/api/v1/items/random_request_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe "random item API" do
+  it "returns a random item object" do
+    create_list(:item, 150)
+    item_ids = []
+
+    Item.all.each do |item|
+      item_ids << item.id
+    end
+
+    get '/api/v1/items/random'
+
+    expect(response).to be_successful
+
+    parse = JSON.parse(response.body)
+    random_items_id_1 = parse["data"]["attributes"]["id"]
+
+    expect(item_ids).to include(random_item_id_1)
+
+    get '/api/v1/items/random'
+    parse = JSON.parse(response.body)
+    random_item_id_2 = parse["data"]["attributes"]["id"]
+
+    expect(item_ids).to include(random_item_id_2)
+    expect(random_item_id_1).to_not eq(random_item_id_2)
+  end
+end

04d937557bb9d01b9f3448a860421a947c214bd7
Dylan Connolly <dylanconnolly145@gmail.com>
2020-02-02 14:35:53 -0700

    Update transaction factory to auto generate an invoice and to sequence credit_card_number

--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :transaction do
-    invoice { nil }
-    credit_card_number { "10239890128301" }
+    invoice
+    sequence(:credit_card_number, 12092389209390) { |n| "#{n}" }
     credit_card_expiration_date { "" }
     result { 0 }
   end

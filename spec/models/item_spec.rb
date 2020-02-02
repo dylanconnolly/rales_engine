b3ad6fc45bbef33f2fb8ce862d2706973a5b73d4
Dylan Connolly <dylanconnolly145@gmail.com>
2020-02-01 22:32:28 -0700

    Update test set up to create invoices with different created dates

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -17,9 +17,27 @@ RSpec.describe Item, type: :model do
     end
 
     top_items = Item.most_revenue(5)
-    
+
     expect(top_items.length).to eq(5)
     expect(top_items.first).to eq(top_item)
     expect(Item.most_revenue(2).length).to eq(2)
+  end
+
+  it "best_day" do
+    item = create(:item)
+
+    create_list(:invoice, 8)
+
+    Invoice.all.each do |invoice|
+      create(:transaction, invoice: invoice)
+      create(:invoice_item, invoice: invoice, item: item)
+      invoice.update(created_at: '2020-01-30')
+    end
+
+    Invoice.all[0].update(created_at: "2020-01-14")
+    Invoice.all[1].update(created_at: "2020-01-14")
+    Invoice.all[2].update(created_at: "2020-01-14")
+
+    expect(Item.best_day(item.id)[0].best_day).to eq('2020-01-30')
   end
 end
--- spec/requests/api/v1/items/best_day_request_spec.rb
+++ spec/requests/api/v1/items/best_day_request_spec.rb
@@ -4,12 +4,17 @@ describe "best day request" do
   it "returns date item had most sales" do
     item = create(:item)
 
-    create_list(:invoice, 5, created_at: '2020-01-30')
-    create_list(:invoice, 3, created_at: '2020-01-14')
+    create_list(:invoice, 8)
 
     Invoice.all.each do |invoice|
       create(:transaction, invoice: invoice)
+      create(:invoice_item, invoice: invoice, item: item)
+      invoice.update(created_at: '2020-01-30')
     end
+
+    Invoice.all[0].update(created_at: "2020-01-14")
+    Invoice.all[1].update(created_at: "2020-01-14")
+    Invoice.all[2].update(created_at: "2020-01-14")
 
     get "/api/v1/items/#{item.id}/best_day"
 

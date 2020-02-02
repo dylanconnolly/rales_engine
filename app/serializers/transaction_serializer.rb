fd7c2b2a582c1da6030181f8d2edc6f3d34cd59e
Dylan Connolly <dylanconnolly145@gmail.com>
2020-02-02 14:35:24 -0700

    Create transaction serializer

---
+++ app/serializers/transaction_serializer.rb
@@ -0,0 +1,7 @@
+class TransactionSerializer
+  include FastJsonapi::ObjectSerializer
+
+  belongs_to :invoice
+
+  attributes :id, :credit_card_number, :credit_card_expiration_date, :result, :invoice_id
+end

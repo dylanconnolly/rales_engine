ff0c88677b2e32d6d7b309c5770391c97d71e6eb
Dylan Connolly <dylanconnolly145@gmail.com>
2020-02-01 19:07:26 -0700

    Add route for items/:id/invoice_items relationship. Remove unneeded brackets around only index calls

--- config/routes.rb
+++ config/routes.rb
@@ -4,30 +4,32 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       namespace :merchants do
-        resources :most_revenue, only: [:index]
-        resources :find, only: [:index]
-        resources :find_all, only: [:index]
-        resources :random, only: [:index]
-        resources :revenue, only: [:index]
+        resources :most_revenue, only: :index
+        resources :find, only: :index
+        resources :find_all, only: :index
+        resources :random, only: :index
+        resources :revenue, only: :index
       end
 
       resources :merchants, only: [:index, :show] do
-
         scope module: 'merchants' do
-          resources :items, only: [:index]
-          resources :invoices, only: [:index]
-          resources :favorite_customer, only: [:index]
+          resources :items, only: :index
+          resources :invoices, only: :index
+          resources :favorite_customer, only: :index
         end
-
       end
 
       namespace :items do
-        resources :find, only: [:index]
-        resources :find_all, only: [:index]
-        resources :random, only: [:index], :controller => "random_item"
+        resources :find, only: :index
+        resources :find_all, only: :index
+        resources :random, only: :index, :controller => "random_item"
       end
 
-      resources :items, only: [:index, :show]
+      resources :items, only: [:index, :show] do
+        scope module: 'items' do
+          resources :invoice_items, only: :index
+        end
+      end
     end
   end
 end

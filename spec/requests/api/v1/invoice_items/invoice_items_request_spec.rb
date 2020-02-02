71aba9f931e25812c75fe62cec4fbc6942afdd29
Dylan Connolly <dylanconnolly145@gmail.com>
2020-02-02 13:35:26 -0700

    Create spec for invoice_items index and show requests

---
+++ spec/requests/api/v1/invoice_items/invoice_items_request_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe 'invoice_items API' do
+  it "returns a list of all invoice_items" do
+    create_list(:invoice_item, 5)
+
+    get '/api/v1/invoice_items'
+
+    expect(response).to be_successful
+
+    invoice_items = JSON.parse(response.body)["data"]
+
+    expect(invoice_items.count).to eq(5)
+  end
+
+  it "returns a single record of invoice_items by id" do
+    invoice_item = create(:invoice_item)
+
+    get "/api/v1/invoice_items/#{invoice_item.id}"
+
+    expect(response).to be_successful
+
+    invoice_item_data = JSON.parse(response.body)["data"]
+
+    expect(invoice_item_data["attributes"]["id"]).to eq(invoice_item_data.id)
+  end
+end

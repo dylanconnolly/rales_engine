13487784dc00153ff1fc293af2052f8816d93846
Dylan Connolly <dylanconnolly145@gmail.com>
2020-02-02 21:52:39 -0700

    Add tests for merchant and customer relationships for invoices

--- spec/requests/api/v1/invoices/relationships_request_spec.rb
+++ spec/requests/api/v1/invoices/relationships_request_spec.rb
@@ -37,4 +37,66 @@ describe "invoice relationships requests" do
       end
     end
   end
+
+  describe "items relationship request" do
+    it "returns collection of items belonging to that invoice" do
+      invoice = create(:invoice)
+      create_list(:invoice_item, 5, invoice_id: invoice.id)
+
+      get "/api/v1/invoices/#{invoice.id}/items"
+
+      expect(response).to be_successful
+
+      items_info = JSON.parse(response.body)["data"]
+
+      expect(items_info.count).to eq(5)
+    end
+  end
+
+  describe "items relationship request" do
+    it "returns collection of items belonging to that invoice" do
+      invoice = create(:invoice)
+      create_list(:invoice_item, 5, invoice_id: invoice.id)
+
+      get "/api/v1/invoices/#{invoice.id}/items"
+
+      expect(response).to be_successful
+
+      items_info = JSON.parse(response.body)["data"]
+
+      expect(items_info.count).to eq(5)
+    end
+  end
+
+  describe "customer relationship request" do
+    it "returns associated customer belonging to that invoice" do
+      customer = create(:customer)
+      invoice = create(:invoice, customer_id: customer.id)
+
+
+      get "/api/v1/invoices/#{invoice.id}/customer"
+
+      expect(response).to be_successful
+
+      customer_info = JSON.parse(response.body)["data"]
+
+      expect(customer_info["attributes"]["id"]).to eq(customer.id)
+    end
+  end
+
+  describe "merchant relationship request" do
+    it "returns associated merchant belonging to that invoice" do
+      merchant = create(:merchant)
+      invoice = create(:invoice, merchant_id: merchant.id)
+
+
+      get "/api/v1/invoices/#{invoice.id}/merchant"
+
+      expect(response).to be_successful
+
+      merchant_info = JSON.parse(response.body)["data"]
+
+      expect(merchant_info["attributes"]["id"]).to eq(merchant.id)
+    end
+  end
 end

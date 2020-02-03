8d734eff453640512d809b4671367b5e611be417
Dylan Connolly <dylanconnolly145@gmail.com>
2020-02-02 20:38:26 -0700

    Create spec for customer transactions relationship request

--- spec/requests/api/v1/customers/relationships_request_spec.rb
+++ spec/requests/api/v1/customers/relationships_request_spec.rb
@@ -19,4 +19,31 @@ describe "customers relationships requests" do
       expect(invoice["attributes"]["customer_id"]).to eq(customer.id)
     end
   end
+
+  it "returns a collection of transactions belonging to that customer" do
+    customer = create(:customer)
+    create_list(:invoice, 5, customer_id: customer.id)
+
+    Invoice.all.each do |invoice|
+      create(:transaction, invoice_id: invoice.id)
+    end
+
+    invoice_ids = Invoice.all.map do |invoice|
+        invoice.id
+    end
+
+    first_transaction = Transaction.first
+
+    get "/api/v1/customers/#{customer.id}/transactions"
+
+    expect(response).to be_successful
+
+    transactions = JSON.parse(response.body)["data"]
+
+    expect(transactions.count).to eq(5)
+    expect(transactions.first["attributes"]["id"]).to eq(first_transaction.id)
+    transactions.each do |transaction|
+      expect(invoice_ids).to include(transaction["attributes"]["invoice_id"])
+    end
+  end
 end

6350417f61317b80b8eb124ae83516f4e2e05398
Dylan Connolly <dylanconnolly145@gmail.com>
2020-02-02 21:52:55 -0700

    Create invoices/merchant and invoices/customer controllers

---
+++ app/controllers/api/v1/invoices/customer_controller.rb
@@ -0,0 +1,8 @@
+class Api::V1::Invoices::CustomerController < ApplicationController
+
+  def index
+    invoice = Invoice.find(params[:invoice_id])
+
+    render json: CustomerSerializer.new(invoice.customer)
+  end
+end
---
+++ app/controllers/api/v1/invoices/merchant_controller.rb
@@ -0,0 +1,8 @@
+class Api::V1::Invoices::MerchantController < ApplicationController
+
+  def index
+    invoice = Invoice.find(params[:invoice_id])
+
+    render json: MerchantSerializer.new(invoice.merchant)
+  end
+end

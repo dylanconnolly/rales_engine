453040a4aaa5b937577bf36e981fac24fb7f6e80
Dylan Connolly <dylanconnolly145@gmail.com>
2020-02-02 20:15:58 -0700

    Create spec for customer invoices relationship request

---
+++ spec/requests/api/v1/customers/relationships_request_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe "customers relationships requests" do
+  it "returns a collection of invoices belonging to customer" do
+    customer = create(:customer)
+    create_list(:invoice, 5, customer_id: customer.id)
+
+    first_invoice = Invoice.first
+
+    get "/api/v1/customers/#{customer.id}/invoices"
+
+    expect(response).to be_successful
+
+    invoices = JSON.parse(response.body)["data"]
+
+    expect(invoices.count).to eq(5)
+    expect(invoices.first["attributes"]["id"]).to eq(first_invoice.id)
+  end
+end
